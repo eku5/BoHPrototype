53586013b4312f89a28b37716c2fedfbf67c1c0c
E. Ung <evee.ung@gmail.com>
2018-04-11 16:24:34 -0400

    Test 1.

---
+++ Assets/Scripts/ChangeBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBackground : MonoBehaviour {
+	public Sprite[] backgroundImages;
+	public SpriteRenderer backgroundSr;
+	private int currentBackgroundIndex;
+	public ExampleVariableStorage variableStorage;
+	public int backgroundIndex;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		backgroundIndex = Mathf.RoundToInt (variableStorage.GetValue ("$backgroundIndex").AsNumber);
+		if (backgroundIndex != currentBackgroundIndex) {
+			currentBackgroundIndex = backgroundIndex;
+			backgroundSr.sprite = backgroundImages [backgroundIndex];
+		}
+	}
+
+}
