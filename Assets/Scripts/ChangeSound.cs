aaf773219a0803c732ff5727473eab1fbb63776c
E. Ung <evee.ung@gmail.com>
2018-05-11 22:18:28 -0400

    Changes to Change Sound script and sounds added.

--- Assets/Scripts/ChangeSound.cs
+++ Assets/Scripts/ChangeSound.cs
@@ -13,9 +13,17 @@ public class ChangeSound : MonoBehaviour {
 	public AudioSource source;
 	//public int soundIndex;
 
+	bool newSoundPlayed;
+	bool newSoundCued;
+	float soundTimer;
+	float soundTimerLength;
 	// Use this for initialization
 	void Start () {
-
+		newSoundCued = false;
+		newSoundPlayed = false;
+		currentSoundIndex = 0;
+		soundTimer = 0f;
+		soundTimerLength = 0f;
 		source = GetComponent<AudioSource> ();
 
 //		if (backgroundSounds != backgroundSound[currentSoundIndex]) {
@@ -29,18 +37,34 @@ public class ChangeSound : MonoBehaviour {
 	void Update () 
 	{
 		//Debug.Log (source.isPlaying);
+
+		//Getting Variable from Yarn
 		currentSoundIndex = Mathf.RoundToInt (variableStorage.GetValue ("$soundIndex").AsNumber);
-		if (backgroundSounds != backgroundSound [currentSoundIndex]) 
+
+		//check if this is a new index, also check that the audio source isn't already playing, and that
+		//we haven't played a sound effect yet
+		if (backgroundSounds != backgroundSound [currentSoundIndex] ) 
 		{
-			backgroundSounds = backgroundSound [currentSoundIndex];
+			Debug.Log ("current sound index changed " + currentSoundIndex);
+			PlayNewSound ();
+			newSoundPlayed = true;
+			soundTimer = 0f;
+			 
+		} 
 
-
+		if (newSoundPlayed == true) {
+			soundTimer += Time.deltaTime;
+			if (soundTimer > soundTimerLength) {
+				newSoundPlayed = false;
+			}
 		}
 
-		if (!source.isPlaying) {
-			source.Play ();
-		}
+	}
 
+	void PlayNewSound() {
+		backgroundSounds = backgroundSound [currentSoundIndex];
 		source.clip = backgroundSounds;
+		soundTimerLength = backgroundSounds.length;
+		source.Play ();
 	}
 }
